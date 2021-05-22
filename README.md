723f18f209b93b53648e3a6f5af25f28000b958d
vardhan <harshwardhankalse03@gmail.com>
2021-05-22 22:23:40 +0530

    5'th Commit : Added scss and mediaQueries

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Movie App Using React js
 
+used scss
+>> you can search movies as well as get their details such as genre plot actors and Released date also
+
--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
 
 const MovieDetails = () => {
+  const [loading, setLoading] = useState(false);
   const { id } = useParams();
   const [searchData, setSearchData] = useState([]);
+
   const fetchMovies = async (id) => {
     const url = `http://www.omdbapi.com/?i=${id}&apikey=b53624b3`;
     const data = await axios
@@ -12,6 +15,7 @@ const MovieDetails = () => {
       .then((res) => {
         setSearchData(res.data);
         console.log(res.data);
+        setLoading(true);
       })
       .catch((err) => console.log(err));
     if (data) {
@@ -23,30 +27,36 @@ const MovieDetails = () => {
     fetchMovies(id);
   }, [id]);
   return (
-    <div className="container_details">
-      <div className="details">
-        <div className="details_img">
-          <img src={searchData.Poster} alt={searchData.Title} />
-        </div>
-        <div className="details_info">
-          <h1>
-            <span className="info_title">Title</span> : {searchData.Title}
-          </h1>
-          <h2>
-            <span className="info_title">Genre</span> : {searchData.Genre}
-          </h2>
-          <p>"{searchData.Plot}"</p>
-          <h3>
-            <span className="info_title">Actors</span> : {searchData.Actors}
-          </h3>
-          <h3>
-            <span className="info_title">Released Date</span> :{" "}
-            {searchData.Released}
-          </h3>
-          <Link to="/">
-            <button>Go Back</button>
-          </Link>
-        </div>
+    <div className="container_details query_details">
+      <div className="details query">
+        {loading ? (
+          <>
+            <div className="details_img">
+              <img src={searchData.Poster} alt={searchData.Title} />
+            </div>
+            <div className="details_info query_info ">
+              <h1>
+                <span className="info_title">Title</span> : {searchData.Title}
+              </h1>
+              <h2>
+                <span className="info_title">Genre</span> : {searchData.Genre}
+              </h2>
+              <p>"{searchData.Plot}"</p>
+              <h3>
+                <span className="info_title">Actors</span> : {searchData.Actors}
+              </h3>
+              <h3>
+                <span className="info_title">Released Date</span> :{" "}
+                {searchData.Released}
+              </h3>
+              <Link to="/">
+                <button>Go Back</button>
+              </Link>
+            </div>
+          </>
+        ) : (
+          <BarLoader color="white" loading />
+        )}
       </div>
     </div>
   );
